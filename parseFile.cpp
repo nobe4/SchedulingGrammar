eda70e64e6c532a202ca4289880ed15d84d84400
nobe4 <haffreinguev@gmail.com>
2014-01-13 10:31:40 +0100

    added testFiles grammar1 & grammar2 (tested) and creation of the adgency matrix in the parseFile function

--- parseFile.cpp
+++ parseFile.cpp
@@ -5,10 +5,28 @@
 #include <vector>
 #include <iterator>
 
-
 using namespace std;
 
-void parseFile(string path);
+// Cell of the adjacency matrix
+// This structure is used to store values in adjacency matrix
+// The field "valid" is null when the value is not valid,
+// and has any other value otherwise.
+// The field "value" is the weight of the link.
+typedef struct _adjCell {
+    int value, valid;
+} AdjCell;
+
+// Link in the graph.
+// This structure is used to store a link.
+// The field "value" is the weight of the link.
+// The field "edge1" is the first extremity of the link.
+// The field "value" is the second extremity of the link.
+typedef struct _link {
+    int value, edge1, edge2;
+} Link;
+
+
+void parseFile(string path, vector<vector<AdjCell> > &adjMatrix);
 // file synthax (tabs are actually spaces)
 // 3                       number of tasks
 // 1   10  2   3   -1      lenght (10) et constraint (2 et 3) of task 1
@@ -16,22 +34,22 @@ void parseFile(string path);
 // 3   30 -1               lenght (30) et constraint (aucune) of task 3
 // -1                      eof
 
-// add a task to the configuration
-void addTask(int taskNb, int lenght, vector<int> constraints);
-
 vector<string> split(string const &input);
 
 // parse string to int and return it
 // careful : 0 is either "0" of an invalid string parsed
 int parseInt(string s);
+
+//print adjency matrix
+void print(const vector<vector<AdjCell> > &adjMatrix, int size);
  
-int main(int argc, char const *argv[])
-{
-    parseFile("grammar.g");
+int main(int argc, char const *argv[]){
+    vector< vector<AdjCell> > adjMatrix;
+    parseFile("grammar.g", adjMatrix);
     return 0;
 }
 
-void parseFile(string path){
+void parseFile(string path, vector<vector<AdjCell> > &adjMatrix){
     
     ifstream file(path.c_str()); // open the file
     
@@ -46,7 +64,7 @@ void parseFile(string path){
     if(file.is_open()){
         // while we get a new line we put it in the line string
         while(getline(file, line)){
-            cout << "lenght(" <<  (split(line)).size() << ") : " << line << endl;
+            cout << endl << "lenght(" <<  (split(line)).size() << ") : " << line << endl;
             //  first line : we get the number of tasks and put it in a variables
             //  if negative or null, return because of error
             if(nbTasks == -1){
@@ -57,6 +75,16 @@ void parseFile(string path){
                     return;
                 }
                 cout << "number of tasks to be parsed : " << nbTasks << endl;
+
+                // resize the adgency matrix and set all value to not valid
+                adjMatrix.resize(nbTasks + 2); // we considere the initial and final task
+                for (int i = 0; i < adjMatrix.size(); ++i){
+                    adjMatrix[i].resize(nbTasks + 2);
+                    for (int j = 0; j < adjMatrix[j].size(); ++j){
+                        adjMatrix[i][j].valid = false;
+                    }
+                }
+
             } else { // not the first turn
                 // we check for the eof
                 if(line == "-1"){
@@ -84,6 +112,12 @@ void parseFile(string path){
                         return;
                     }
 
+                    // check if the line contains at least 3 elements : task number length and -1
+                    if(splittedLine.size() < 3){
+                        cout << "error : line too short !" << endl;
+                        return;
+                    }
+
                     // verify that the last one is "-1"
                     if(splittedLine[splittedLine.size() -1] != "-1"){
                         cout << "error : missing -1 at the end of the line" << endl;
@@ -96,13 +130,62 @@ void parseFile(string path){
                     for (int i = 2; i < splittedLine.size() - 1 ; ++i) {
                         constraints.push_back(parseInt(splittedLine[i]));
                     }
-                    cout <<  constraints.size() << " constraints parsed"<< endl;
-                    addTask(parseInt(splittedLine[0]),parseInt(splittedLine[0]),constraints);
+                    cout <<  constraints.size() << " constraints parsed !"<< endl;
+                    
+                    // set the line to the length
+                    for (int i = 0; i < adjMatrix.size(); ++i) {
+                        adjMatrix[parseInt(splittedLine[0])][i].value = parseInt(splittedLine[1]);
+                    }
 
+                    // if there is no constraint we set the edge to be the successor of the initial edge
+                    if(constraints.size() == 0){
+                        cout << "task n°" << parseInt(splittedLine[0]) << " of lenght " << parseInt(splittedLine[1]) << " without constraint "<< endl;
+                        adjMatrix[0][parseInt(splittedLine[0])].valid = true;
+                    }else{
+                        // add the constraint to the adjency matrix
+                        for (int i = 0; i < constraints.size(); ++i){
+                            cout << "task n°" << parseInt(splittedLine[0]) << " of lenght " << parseInt(splittedLine[1]) << " with constraint " << constraints[i] << " added !" << endl;
+                            adjMatrix[constraints[i]][parseInt(splittedLine[0])].valid = true;
+                        }
+                    }
                     nbTasksParsed ++;
                 }
             }
         }
+        // after we parsed the adjency matrix 
+
+        // we add all edge without successor to be predecessor of final edge
+        for (int i = 1; i < adjMatrix.size() - 1; ++i) {
+            bool filled = false;
+            for (int j = 1; j < adjMatrix.size() - 1; ++j){
+                filled = adjMatrix[i][j].valid || filled;
+            }
+
+            //if not filled then the edge has no successor
+            if(!filled){
+                cout << "node " << i << " has no sucessor : we add it to the predecessors of the final node" << endl;
+                adjMatrix[i][adjMatrix.size() - 1].valid = true;
+            }
+        }
+
+        // we add all edge without predecessors to be successor of initial edge
+        for (int i = 1; i < adjMatrix.size() - 1; ++i) {
+            bool filled = false;
+            for (int j = 1; j < adjMatrix.size() - 1; ++j){
+                filled = adjMatrix[j][i].valid || filled;
+            }
+
+            //if not filled then the edge has no successor
+            if(!filled){
+                cout << "node " << i << " has no predecessor : we add it to the successors of the initial node" << endl;
+                adjMatrix[0][i].valid = true;
+            }
+        }
+
+        // print the matrix created
+        cout << "Matrix created : " << endl;
+        print(adjMatrix,adjMatrix.size());
+
         // we close the file
         file.close();
     }
@@ -122,7 +205,19 @@ int parseInt(string s){
     return r;
 }
 
-void addTask(int taskNb, int lenght, vector<int> constraints){
-    cout << "task n°" << taskNb << " of lenght " << lenght << " with " << constraints.size() << " constraints added !" << endl;
+void print(const vector<vector<AdjCell> > &adjMatrix, int size) {
+    for (int j = 0; j < size; ++j) {
+        cout << "\t" << j;
+    }
+    for (int i = 0; i < size; ++i) {
+        cout << endl << i << "\t";
+        for (int j = 0; j < size; ++j) {
+            if (adjMatrix[i][j].valid) {
+                cout << adjMatrix[i][j].value << "\t";
+            } else {
+                cout << ".\t";
+            }
+        }
+    }
+    cout << endl;
 }
-

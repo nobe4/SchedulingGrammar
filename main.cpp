1cbb4f3fcef85fae3ea2f5a6f4e6857524c937ec
Matthieu <matthieudelaro@gmail.com>
2014-01-13 11:08:08 +0100

    Merge

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 //
 
 #include <iostream>
-#include <cstdio>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iterator>
 
 using namespace std;
 
@@ -60,51 +63,28 @@ void computeDates(const AdjMatrix &adjMatrix, vector<int> &earliestDates, vector
  */
 void printCalendar(vector<int> &earliestDates, vector<int> &latestDates);
 
-bool circuit(const AdjMatrix &adjMatrix) {
-    // TODO
-    return true;
-}
+// split the input string from console with whitespaces
+vector<string> split(string const &input);
 
-bool executeCommand(AdjMatrix adjMatrix) {
-    string s;
-    bool goOn = true;
-    while(goOn) {
-        getline(cin, s);
-        if (s == "stop" || s == "s") {
-            goOn = false;
-            return false;
-        } /*else if {
-            // TODO
-            // other options
-        }*/
-    }
-    return true;
-}
-
-void computeDates(const AdjMatrix &adjMatrix, vector<int> &earliestDates, vector<int> &latestDates) {
-    // TODO
-}
-
-void printCalendar(vector<int> &earliestDates, vector<int> &latestDates) {
-    // TODO
-}
-
-void parseFile(string path, AdjMatrix &adjMatrix) {
-    // TODO
-}
+// parse string to int and return it
+// careful : 0 is either "0" of an invalid string parsed
+int parseInt(string s);
 
 int main(int argc, const char * argv[]) {
     string path;
-    if (argc == 2) {
-        path = argv[1];
-    } else {
-        cout << "Which file do you want to open ? ";
-        cin >> path;
-        cout << endl;
-    }
+//    if (argc == 2) {
+//        path = argv[1];
+//    } else {
+//        cout << "Which file do you want to open ? ";
+//        cin >> path;
+//        cout << endl;
+//    }
 //    path = "/Users/matthieudelaro/Documents/classes/L3/S5/theorie_des_graphes/TP/tp33/Haffreingue-de_La_Roche_Saint_Andre-EFREI-L3-TG-TP3-5.txt";
+    path = "/Users/matthieudelaro/Documents/classes/L3/S5/theorie_des_graphes/TP/tp4/grammar.g";
+//    path = "grammar.g";
     
     AdjMatrix adjMatrix;
+    cout << "Parsing file " << path << " :" << endl;
     parseFile(path, adjMatrix);
     
     bool goOn = true;
@@ -128,6 +108,194 @@ int main(int argc, const char * argv[]) {
         }
     }
 }
+
+bool circuit(const AdjMatrix &adjMatrix) {
+    // TODO
+    return true;
+}
+
+bool executeCommand(AdjMatrix adjMatrix) {
+    string s;
+    bool goOn = true;
+    while(goOn) {
+        getline(cin, s);
+        if (s == "stop" || s == "s") {
+            goOn = false;
+            return false;
+        } /*else if {
+           // TODO : get inspiration from parseInput
+           // other options
+           }*/
+    }
+    return true;
+}
+
+void computeDates(const AdjMatrix &adjMatrix, vector<int> &earliestDates, vector<int> &latestDates) {
+    // TODO
+}
+
+void printCalendar(vector<int> &earliestDates, vector<int> &latestDates) {
+    // TODO
+}
+
+void parseFile(string path, AdjMatrix &adjMatrix) {
+    ifstream file(path.c_str()); // open the file
+    
+    string line = ""; // buffer that will contains every line once at the time
+    
+    int nbTasks = -1;       // number of tasks found on first line
+    int nbTasksParsed = 0;  // number of tasks parsed to make a verification
+    
+    vector<string> splittedLine;   // default value to prevent cross OS errors ? ###############################
+    
+    // if the file successfully opened
+    if(file.is_open()){
+        // while we get a new line we put it in the line string
+        while(getline(file, line)){
+            cout << endl << "lenght(" <<  (split(line)).size() << ") : " << line << endl;
+            //  first line : we get the number of tasks and put it in a variables
+            //  if negative or null, return because of error
+            if(nbTasks == -1){
+                // we cast the line to int
+                nbTasks = parseInt(line);
+                if(nbTasks < 1){
+                    cout << "error : bad number of tasks !" << endl;
+                    return;
+                }
+                cout << "number of tasks to be parsed : " << nbTasks << endl;
+                
+                // resize the adgency matrix and set all value to not valid
+                adjMatrix.resize(nbTasks + 2); // we considere the initial and final task
+                for (int i = 0; i < adjMatrix.size(); ++i){
+                    adjMatrix[i].resize(nbTasks + 2);
+                    for (int j = 0; j < adjMatrix[j].size(); ++j){
+                        adjMatrix[i][j].valid = false;
+                    }
+                }
+                
+            } else { // not the first turn
+                // we check for the eof
+                if(line == "-1"){
+                    cout << "end of file detected" << endl;
+                    // process the end of file :
+                    // (1) check the number of parsed tasks
+                    if(nbTasksParsed != nbTasks){
+                        cout << "error : not right number of tasks : " << nbTasksParsed << " instead of " << nbTasks << " !" << endl;
+                        return;
+                    }
+                    
+                    // (2) what to do next ? ##############################
+                    
+                    cout << "right number of tasks parsed !" << endl;
+                }
+                else{ // if not end of file
+                    cout << "new line to be splitted : " << line << endl;
+                    // we split the line into a vector to get each
+                    splittedLine = split(line);
+                    cout << splittedLine.size() << " symbols splitten" << endl;
+                    
+                    // check if the line is not empty
+                    if(splittedLine.size() == 0){
+                        cout << "error : line empty !" << endl;
+                        return;
+                    }
+                    
+                    // check if the line contains at least 3 elements : task number length and -1
+                    if(splittedLine.size() < 3){
+                        cout << "error : line too short !" << endl;
+                        return;
+                    }
+                    
+                    // verify that the last one is "-1"
+                    if(splittedLine[splittedLine.size() -1] != "-1"){
+                        cout << "error : missing -1 at the end of the line" << endl;
+                        return;
+                    }
+                    
+                    // someting else to check directly ? ############################
+                    
+                    vector<int> constraints;
+                    for (int i = 2; i < splittedLine.size() - 1 ; ++i) {
+                        constraints.push_back(parseInt(splittedLine[i]));
+                    }
+                    cout <<  constraints.size() << " constraints parsed !"<< endl;
+                    
+                    // set the line to the length
+                    for (int i = 0; i < adjMatrix.size(); ++i) {
+                        adjMatrix[parseInt(splittedLine[0])][i].value = parseInt(splittedLine[1]);
+                    }
+                    
+                    // if there is no constraint we set the edge to be the successor of the initial edge
+                    if(constraints.size() == 0){
+                        cout << "task n°" << parseInt(splittedLine[0]) << " of lenght " << parseInt(splittedLine[1]) << " without constraint "<< endl;
+                        adjMatrix[0][parseInt(splittedLine[0])].valid = true;
+                    }else{
+                        // add the constraint to the adjency matrix
+                        for (int i = 0; i < constraints.size(); ++i){
+                            cout << "task n°" << parseInt(splittedLine[0]) << " of lenght " << parseInt(splittedLine[1]) << " with constraint " << constraints[i] << " added !" << endl;
+                            adjMatrix[constraints[i]][parseInt(splittedLine[0])].valid = true;
+                        }
+                    }
+                    nbTasksParsed ++;
+                }
+            }
+        }
+        // after we parsed the adjency matrix
+        
+        // we add all edge without successor to be predecessor of final edge
+        for (int i = 1; i < adjMatrix.size() - 1; ++i) {
+            bool filled = false;
+            for (int j = 1; j < adjMatrix.size() - 1; ++j){
+                filled = adjMatrix[i][j].valid || filled;
+            }
+            
+            //if not filled then the edge has no successor
+            if(!filled){
+                cout << "node " << i << " has no sucessor : we add it to the predecessors of the final node" << endl;
+                adjMatrix[i][adjMatrix.size() - 1].valid = true;
+            }
+        }
+        
+        // we add all edge without predecessors to be successor of initial edge
+        for (int i = 1; i < adjMatrix.size() - 1; ++i) {
+            bool filled = false;
+            for (int j = 1; j < adjMatrix.size() - 1; ++j){
+                filled = adjMatrix[j][i].valid || filled;
+            }
+            
+            //if not filled then the edge has no successor
+            if(!filled){
+                cout << "node " << i << " has no predecessor : we add it to the successors of the initial node" << endl;
+                adjMatrix[0][i].valid = true;
+            }
+        }
+        
+        // print the matrix created
+        cout << "Matrix created : " << endl;
+        print(adjMatrix);
+        
+        // we close the file
+        file.close();
+    }
+    
+}
+
+
+
+vector<string> split(string const &input) {
+    istringstream buffer(input);
+    vector<string> ret((istream_iterator<string>(buffer)),istream_iterator<string>());
+    return ret;
+}
+
+int parseInt(string s){
+    istringstream buffer(s);
+    int r = 0;
+    buffer >> r;
+    return r;
+}
+
+
 
 void print(const vector<vector<AdjCell> > &adjMatrix) {
     long size = adjMatrix.size();
